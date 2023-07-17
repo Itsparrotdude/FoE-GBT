7e79e35686a1ba9946b666e61f3b3c3e25a46547
Itsparrotdude <noah.volante@gmail.com>
2023-07-18 11:07:00 +1200

    Added injecting code

---
+++ js/inject.js
@@ -0,0 +1,47 @@
+function promisedLoadCode(src, base = "base") {
+    return new Promise(async (resolve, reject) => {
+      let sc = document.createElement("script");
+      sc.src = src;
+      if (scripts[base]) {
+        scripts[base].push(src);
+      }
+      sc.addEventListener("load", function () {
+        if (scripts[base]) scriptLoaded(src, base);
+        this.remove();
+        resolve();
+      });
+      sc.addEventListener("error", function () {
+        console.error("error loading script " + src);
+        this.remove();
+        reject();
+      });
+      while (!document.head && !document.documentElement)
+        await new Promise((resolve) => {
+          // @ts-ignore
+          requestIdleCallback(resolve);
+        });
+      (document.head || document.documentElement).appendChild(sc);
+    });
+  }
+
+  function scriptLoaded(src, base) {
+    scripts[base].splice(scripts[base].indexOf(src), 1);
+    if (scripts.internal.length == 1) {
+      scripts.internal.splice(scripts.internal.indexOf("once"), 1);
+      window.dispatchEvent(new CustomEvent("foe-helper#loaded"));
+    }
+    // Add more logic to handle other events if needed
+  }
+
+  function inject(loadBeta = false, extUrl = chrome.runtime.getURL(""), betaDate = "") {
+    // Your implementation of the inject function
+    // ...
+  
+    // Call your promisedLoadCode function to load scripts
+    promisedLoadCode(extUrl + "path/to/your_script.js").then(() => {
+      // Script loaded successfully, you can execute any code that depends on the loaded script here.
+    });
+  
+    // ...
+  }
+  
