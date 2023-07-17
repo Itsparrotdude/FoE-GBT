0abef4c4ea6ecac6320abcbe178751c6161ec890
Itsparrotdude <noah.volante@gmail.com>
2023-07-18 11:55:56 +1200

    Testing

--- js/content.js
+++ js/content.js
@@ -1,1 +1,13 @@
-console.log("injects.js injected correctly")
+// content.js
+
+// Function to check if the extension is properly injected
+function checkInjection() {
+  if (document.documentElement.hasAttribute("data-foe-helper-injected")) {
+    console.log("Forge of Empires Helper Extension has been successfully injected!");
+  } else {
+    console.error("Forge of Empires Helper Extension failed to inject properly.");
+  }
+}
+
+// Call the checkInjection function when the document is ready
+document.addEventListener("DOMContentLoaded", checkInjection);
