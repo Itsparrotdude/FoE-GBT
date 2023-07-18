49815cb71ae6c8ec7c253e60d43473a374ca5421
Itsparrotdude <noah.volante@gmail.com>
2023-07-18 15:08:16 +1200

    leroy

--- js/content.js
+++ js/content.js
@@ -1,5 +1,3 @@
-// content.js
-
 // Function to check if the extension is properly injected
 function checkInjection() {
   if (document.documentElement.hasAttribute("data-foe-helper-injected")) {
@@ -8,12 +6,6 @@ function checkInjection() {
     console.error("Forge of Empires Helper Extension failed to inject properly.");
   }
 }
-
-// Call the checkInjection function when the document is ready
-document.addEventListener("DOMContentLoaded", checkInjection);
-
-chrome.extension.getURL('leroy.png')
-
 
 // Function to load HTML file using Fetch API
 async function loadHtmlFile(fileUrl) {
@@ -44,12 +36,18 @@ function insertHtmlIntoPage(htmlContent) {
   document.body.appendChild(div);
 }
 
-// Usage: Load and insert the blue_box.html content into the page
-const htmlFileUrl = chrome.runtime.getURL('html/popup2.html');
-loadHtmlFile(htmlFileUrl)
-  .then((htmlContent) => {
-    insertHtmlIntoPage(htmlContent);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// Call the checkInjection function when the document is ready
+document.addEventListener("DOMContentLoaded", () => {
+  checkInjection();
+  
+  // Usage: Load and insert the html/popup2.html content into the page
+  const htmlFileUrl = chrome.runtime.getURL('html/popup2.html');
+  loadHtmlFile(htmlFileUrl)
+    .then((htmlContent) => {
+      insertHtmlIntoPage(htmlContent);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
