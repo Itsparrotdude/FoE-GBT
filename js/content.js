98ac62051e9d8d355b115cfcf25756bda3b05bf2
parthsetiya <132999404+parthsetiya@users.noreply.github.com>
2023-07-18 20:58:01 +1200

    got image to show on top of game

--- js/content.js
+++ js/content.js
@@ -47,3 +47,65 @@ document.addEventListener("DOMContentLoaded", () => {
       console.error(error);
     });
 });
+
+function createOverlay() {
+  const overlay = document.createElement('div');
+  overlay.style.position = 'absolute';
+  overlay.style.top = '0';
+  overlay.style.left = '0';
+  overlay.style.width = '100%';
+  overlay.style.height = '100%';
+  overlay.style.zIndex = '9999'; // A higher value to appear on top of the canvas
+
+  document.body.appendChild(overlay);
+  return overlay;
+}
+
+// Function to insert the loaded HTML content into the DOM
+function insertHtmlIntoPage(htmlContent) {
+  // ... (your existing code for insertHtmlIntoPage)
+
+  // Create the overlay
+  const overlay = createOverlay();
+
+  // Create the image element
+  const imageElement = document.createElement("img");
+  // Directly use the image URL (since it's web_accessible_resource)
+  imageElement.src = chrome.runtime.getURL("images/leroy.png");
+  // Position the image on the canvas (adjust top and left as needed)
+  imageElement.style.position = 'absolute';
+  imageElement.style.top = '50px';
+  imageElement.style.left = '50px';
+
+  // Append the image to the overlay
+  overlay.appendChild(imageElement);
+}
+
+
+// Function to insert the loaded HTML content into the DOM
+function insertHtmlIntoPage(htmlContent) {
+  // ... (your existing code for insertHtmlIntoPage)
+
+  // Create the overlay
+  const overlay = createOverlay();
+
+  // Create the image element
+  const imageElement = document.createElement("img");
+  // Directly use the image URL (since it's web_accessible_resource)
+  imageElement.src = chrome.runtime.getURL("images/leroy.png");
+  // Position the image on the canvas (adjust top and left as needed)
+  imageElement.style.position = 'absolute';
+  imageElement.style.top = '50px';
+  imageElement.style.left = '50px';
+
+  // Append the image to the overlay
+  overlay.appendChild(imageElement);
+
+  // Adjust overlay position if the game uses iframes
+  const gameIframe = document.querySelector("iframe"); // Update this selector based on the actual iframe element used by the game
+  if (gameIframe) {
+    const iframeRect = gameIframe.getBoundingClientRect();
+    overlay.style.top = iframeRect.top + 'px';
+    overlay.style.left = iframeRect.left + 'px';
+  }
+}
