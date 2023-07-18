ff4ed5d4e3ddddff319193f49266686f5ccd77dd
Itsparrotdude <noah.volante@gmail.com>
2023-07-18 13:51:58 +1200

    Working Injection

--- js/inject.js
+++ js/inject.js
@@ -1,4 +1,14 @@
-function promisedLoadCode(src, base = "base") {
+// inject.js
+
+// Define the scripts variable at the beginning
+let scripts = {
+    main: ["once", "primed"],
+    proxy: ["once", "primed"],
+    vendor: ["once", "primed"],
+    internal: ["once", "primed"]
+  };
+  
+  function promisedLoadCode(src, base = "base") {
     return new Promise(async (resolve, reject) => {
       let sc = document.createElement("script");
       sc.src = src;
@@ -11,7 +21,7 @@ function promisedLoadCode(src, base = "base") {
         resolve();
       });
       sc.addEventListener("error", function () {
-        console.error("error loading script " + src);
+        console.error("Error loading script " + src);
         this.remove();
         reject();
       });
@@ -23,7 +33,7 @@ function promisedLoadCode(src, base = "base") {
       (document.head || document.documentElement).appendChild(sc);
     });
   }
-
+  
   function scriptLoaded(src, base) {
     scripts[base].splice(scripts[base].indexOf(src), 1);
     if (scripts.internal.length == 1) {
@@ -32,16 +42,20 @@ function promisedLoadCode(src, base = "base") {
     }
     // Add more logic to handle other events if needed
   }
-
+  
   function inject(loadBeta = false, extUrl = chrome.runtime.getURL(""), betaDate = "") {
     // Your implementation of the inject function
     // ...
-  
+    
     // Call your promisedLoadCode function to load scripts
     promisedLoadCode(extUrl + "js/content.js").then(() => {
       // Script loaded successfully, you can execute any code that depends on the loaded script here.
+      console.log("Content.js has been successfully injected!");
     });
-  
+    
     // ...
   }
+  
+  // Call the inject function when the document is ready
+  document.addEventListener("DOMContentLoaded", inject);
   
